8d8cd47f0fe06c3e11df825c095542738ca3fdea
scastillo <castillobuiles@gmail.com>
2015-12-07 18:10:38 -0500

    Better java default map

--- recipes/datastax.rb
+++ recipes/datastax.rb
@@ -31,10 +31,12 @@ end
 
 # Insall Java from cookbook
 #   by default use oracle 8
-
-default["java"]["install_flavor"] ||= "oracle"
-default["java"]["jdk_version"] ||= '8'
-default["java"]["oracle"]["accept_oracle_download_terms"] ||= true
+if not default["java"]
+  default["java"] = {}
+  default["java"]["install_flavor"] = "oracle"
+  default["java"]["jdk_version"] = '8'
+  default["java"]["oracle"] = {"accept_oracle_download_terms" => true}
+end
 
 include_recipe 'java'
 
