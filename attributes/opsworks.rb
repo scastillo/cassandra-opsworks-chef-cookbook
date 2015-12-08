8b8d000532fa0d2dd1fb739eabb74ed11150d76a
scastillo <castillobuiles@gmail.com>
2015-12-08 03:43:01 -0500

    Fixed jre file

--- attributes/opsworks.rb
+++ attributes/opsworks.rb
@@ -51,7 +51,7 @@ default[:java] = {
   'jdk' => {
     '8' => {
       'x86_64' => {
-        'url' => 'https://s3.amazonaws.com/setup-dependencies-repo/jre-8u66-linux-x64.tar.gz',
+        'url' => 'https://s3.amazonaws.com/setup-dependencies-repo/jre-8u66-linux-x64.gz',
         'checksum' => '88f31f3d642c3287134297b8c10e61bf'
       }
     }
