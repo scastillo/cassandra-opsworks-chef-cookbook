ee21993e3ba2bf81bd04409c21070590ca06f33c
scastillo <castillobuiles@gmail.com>
2015-12-08 01:49:54 -0500

    Fixed integer symbol as map key

--- attributes/opsworks.rb
+++ attributes/opsworks.rb
@@ -45,11 +45,11 @@ default[:java] = {
   :oracle => {
     :accept_oracle_download_terms => true
   },
-  :jdk => {
-    :8 => {
-      :x86_64 => {
-        :url => 'https://s3.amazonaws.com/setup-dependencies-repo/jre-8u66-linux-x64.tar.gz',
-        :checksum => '88f31f3d642c3287134297b8c10e61bf'
+  'jdk' => {
+    '8' => {
+      'x86_64' => {
+        'url' => 'https://s3.amazonaws.com/setup-dependencies-repo/jre-8u66-linux-x64.tar.gz',
+        'checksum' => '88f31f3d642c3287134297b8c10e61bf'
       }
     }
   }
